8ca1dd13c162ef5dff9806bd49bb768ec7c5ee2f
Uladzimiro <dvaraf@gmail.com>
2012-04-30 18:18:11 +0300

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user#user_path(@user.id)
+      redirect_to @user
     else
       render 'new'
     end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -47,6 +47,8 @@ describe "User Pages" do
         
         it { should have_selector('title', text: user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        
+        it { should have_link('Sign out') }
       end
     end
   end
