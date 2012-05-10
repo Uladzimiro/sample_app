875198b67157964d1b7a18015b9dba90d8e8e556
Uladzimiro <dvaraf@gmail.com>
2012-05-10 20:45:35 +0300

    Add user microposts3

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ SampleApp::Application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
+  
+  resources :microposts, only: [:create, :destroy]
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
